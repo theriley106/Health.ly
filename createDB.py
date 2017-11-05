b40e75f312df8e3edfeac857f68a177e17c3c56a
Theriley106 <christopherlambert106@gmail.com>
2017-11-05 01:14:12 -0500

    init

--- createDB.py
+++ createDB.py
@@ -2,7 +2,11 @@ import json
 import threading
 import requests
 import bs4
+import random
 
+r = requests.post("http://138.197.123.15:8888/proxies/{}".format(open('../../SecretCode.txt').read().strip())).json()
+Proxies = r["proxies"]
+print Proxies
 def inputJson(jsonfile):
 	with open(jsonfile) as json_data:
 		d = json.load(json_data)
@@ -12,8 +16,19 @@ def returnIngredients(dpnum):
 	rea = []
 	headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 	url = 'https://www.amazon.com/dp/{}'.format(dpnum)
-	a = requests.get(url, headers=headers)
-	page = bs4.BeautifulSoup(a.text, 'lxml')
+	try:
+		prx = random.choice(Proxies)
+		print prx
+		proxies = {'http': prx, 'https': prx}
+		a = requests.get(url, headers=headers, proxies=proxies)
+		page = bs4.BeautifulSoup(a.text, 'lxml')
+	except:
+		prx = random.choice(Proxies)
+		print prx
+		proxies = {'http': prx, 'https': prx}
+		a = requests.get(url, headers=headers, proxies=proxies)
+		page = bs4.BeautifulSoup(a.text, 'lxml')
+	print page.title.string
 	box = page.select('#importantInformation .content')
 	if 'Ingredients</h5>' in str(box):
 		a = str(box).partition('Ingredients</h5>')[2].partition('<br/>')[0]
@@ -25,20 +40,31 @@ def returnIngredients(dpnum):
 	if len(rea) > 2:
 		return rea
 
+def chunks(l, n):
+    n = max(1, n)
+    return (l[i:i+n] for i in xrange(0, len(l), n))
+
+
 
 def scrapeListOfASIN(key):
-	listofASIN = allASIN[key]
+	listofASI = allASIN[key]
+	listofASIN = chunks(listofASI, 5)
 	info = []
+	def doASINstuff(asin):
+		res = returnIngredients(asin)
+		if res != None:
+			for e in res:
+				info.append(e)
 	for i, asin in enumerate(listofASIN, 1):
 		try:
-			res = returnIngredients(asin)
-			if res != None:
-				for e in res:
-					info.append(e)
-			if i % 10 == 0 or i == 1:
-				with open('{}.json'.format(key), 'w') as fp:
-					json.dump({key: info}, fp)
-				print("Updated: {} with {} items".format(key, i))
+			threads = [threading.Thread(target=doASINstuff, args=(key,)) for key in asin]
+			for thread in threads:
+				thread.start()
+			for thread in threads:
+				thread.join()
+			with open('{}.json'.format(key), 'w') as fp:
+				json.dump({key: info}, fp)
+			print("Updated: {} with {} items".format(key, i*10))
 		except Exception as exp:
 			print exp
 
